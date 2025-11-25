ecbe2493a99d028b26bdbfcf5b6539a54cd793f6
vuhongdang18092004 <hongdang18092004@gmail.com>
2025-11-25 21:50:25 +0700

    Integrate SCSS support for Handlebars templates

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,8 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
