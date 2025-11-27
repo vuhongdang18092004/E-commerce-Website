3a46f2c37481a5dfc8b46e41893e5d1fe45abcb1
vuhongdang18092004 <hongdang18092004@gmail.com>
2025-11-27 14:58:46 +0700

    Learn basic routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,10 +12,15 @@ app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/new", (req, res) => {
+  console.log(req.query);
+  res.render("new");
 });
 
 app.listen(port, () => {
