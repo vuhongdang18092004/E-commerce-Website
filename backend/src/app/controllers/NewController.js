ce38d57fd9b2a9c835f8c2d9337d64eb8fd22385
vuhongdang18092004 <hongdang18092004@gmail.com>
2025-12-08 22:49:52 +0700

    feat: display data with Handlebars

--- backend/src/app/controllers/NewController.js
+++ backend/src/app/controllers/NewController.js
@@ -1,13 +1,21 @@
 const Course = require('../models/Course');
 
 class NewController {
-  async index(req, res) {
-    try {
-      const courses = await Course.find({});
-      res.json(courses);
-    } catch (error) {
-      res.status(400).json({ error: 'ERROR!!!' });
-    }
+  // async index(req, res, next) {
+  //   try {
+  //     const courses = await Course.find({});
+  //     res.json(courses);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // }
+
+  index(req,res, next) {
+    Course.find({})
+      .then(courses => res.render('new',{
+        courses: courses.map(course => course.toObject()),
+      }))
+      .catch(next);
   }
 
   show(req, res) {
